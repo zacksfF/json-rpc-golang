99d8aaa88bcedbb28d2c456d98ce25b422b8edad
zacksfF <ssssaasss571@gmail.com>
2025-03-13 00:53:22 +0000

    fix rpc methods

--- rpc.go
+++ rpc.go
@@ -86,30 +86,38 @@ func SendRequest(method string, params []interface{}) (json.RawMessage, error) {
 }
 
 // Ethereum JSON-RPC Methods
+
+// Returns the latest block number.
 func EthBlockNumber() (json.RawMessage, error) {
 	return SendRequest("eth_blockNumber", nil)
 }
 
+// Gets details of a block using its number.
 func EthGetBlockByNumber(blockNumber string) (json.RawMessage, error) {
 	return SendRequest("eth_getBlockByNumber", []interface{}{blockNumber, true})
 }
 
+// Gets details of a block using its hash.
 func EthGetBlockByHash(blockHash string) (json.RawMessage, error) {
 	return SendRequest("eth_getBlockByHash", []interface{}{blockHash, true})
 }
 
+// Fetches event logs based on filters (address, topics
 func EthGetLogs(filter interface{}) (json.RawMessage, error) {
 	return SendRequest("eth_getLogs", []interface{}{filter})
 }
 
+// Returns the balance of an Ethereum address.
 func EthGetBalance(address string) (json.RawMessage, error) {
 	return SendRequest("eth_getBalance", []interface{}{address, "latest"})
 }
 
+// Returns the number of transactions sent from an address.
 func EthGetTransactionCount(address string) (json.RawMessage, error) {
 	return SendRequest("eth_getTransactionCount", []interface{}{address, "latest"})
 }
 
+// Simulates a smart contract function call (read-only)
 func EthCall(toAddress string, data string) (json.RawMessage, error) {
 	params := []interface{}{map[string]interface{}{
 		"to":   toAddress,
@@ -118,6 +126,7 @@ func EthCall(toAddress string, data string) (json.RawMessage, error) {
 	return SendRequest("eth_call", params)
 }
 
+// Estimates the gas needed for a transaction.
 func EthEstimateGas(toAddress string, data string) (json.RawMessage, error) {
 	params := []interface{}{map[string]interface{}{
 		"to":   toAddress,
@@ -126,18 +135,22 @@ func EthEstimateGas(toAddress string, data string) (json.RawMessage, error) {
 	return SendRequest("eth_estimateGas", params)
 }
 
+// Returns the current gas price in Wei.
 func EthGasPrice() (json.RawMessage, error) {
 	return SendRequest("eth_gasPrice", nil)
 }
 
+// Broadcasts a signed transaction to the network.
 func EthSendRawTransaction(rawTx string) (json.RawMessage, error) {
 	return SendRequest("eth_sendRawTransaction", []interface{}{rawTx})
 }
 
+// Fetches details of a transaction using its hash.
 func EthGetTransactionByHash(txHash string) (json.RawMessage, error) {
 	return SendRequest("eth_getTransactionByHash", []interface{}{txHash})
 }
 
+// Returns the ID of the connected Ethereum network.
 func EthChainId() (json.RawMessage, error) {
 	return SendRequest("eth_chainId", nil)
 }
